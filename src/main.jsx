39b8dc7cac0be9fb84aa66ffb661a1f0ed529296
Svetlana Birthisel <st.seraphim@protonmail.com>
2026-07-06 12:12:51 -0400

    Route /via-latina to Latin tutor

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ReadingApp from './reading/ReadingApp.jsx'
-import SoundsBoard from './reading/SoundsBoard.jsx'
-import Home from './pages/Home.jsx'
-import Latin from './pages/Latin.jsx'
-import MagisterNatura from './pages/MagisterNatura.jsx'
-import LessonViewer from './pages/LessonViewer.jsx'
-import ScholaCantorum from './pages/ScholaCantorum.jsx'
-import RegentNikolay from './pages/RegentNikolay.jsx'
-import MagisterHistoria from "./pages/MagisterHistoria"
-import MagisterArtis from "./pages/MagisterArtis.jsx"
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/read" element={<ReadingApp />} />
-        <Route path="/sounds" element={<SoundsBoard />} />
-        <Route path="/latin" element={<Latin />} />
-        <Route path="/magister-natura" element={<MagisterNatura />} />
-        <Route path="/lesson-viewer" element={<LessonViewer />} />
-        <Route path="/schola-cantorum" element={<ScholaCantorum />} />
-        <Route path="/regent-nikolay" element={<RegentNikolay />} />
-        <Route path="/history" element={<MagisterHistoria />} />
-        <Route path="/magister-artis" element={<MagisterArtis />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 )
